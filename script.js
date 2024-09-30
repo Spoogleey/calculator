ee5fb54f70fa4014e175200315f2a55ed253d252
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-30 14:40:57 +0100

    Created the calculator
    Added the divs and the buttons for the calculator.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let number1;
 let number2;
 let operator;
-
+// Adding
 function add(a, b) {
     return a + b;
 }
-
+// Subtracting
 function subtract(a, b) {
     return a - b;
 }
-
+// Multiplying
 function multiply(a, b) {
     return a * b;
 }
-
+// Dividing
 function divide(a, b) {
     return a / b;
 }
-
+// Take the two numbers and perform the operation
 function operate(num1, num2, oper) {
     let answer = 0;
     switch(oper) {
