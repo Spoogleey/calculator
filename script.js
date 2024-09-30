1d18a9e933e8a43485af2b44efc4f7c7d21c3bf1
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-30 14:20:04 +0100

    Created the "operate" function
    The "operate" function will take in two numbers and an operator. It will then perform one of the mathematical functions based on the operator provided and return the answer.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let number1;
+let number2;
+let operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -13,3 +17,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num1, num2, oper) {
+    let answer = 0;
+    switch(oper) {
+        case "+":
+            answer = add(num1, num2);
+            break;
+        case "-":
+            answer = subtract(num1, num2);
+            break;
+        case "*":
+            answer = multiply(num1, num2);
+            break;
+        case "/":
+            answer = divide(num1, num2);
+            break;
+    }
+    return answer;
+}
+
+console.log(operate(4, 5, "/"));
