cbf9ffa8fc0168f3f307b5cdf338e025bb149045
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-09-30 16:09:26 +0100

    Started getting the calculator working

--- script.js
+++ script.js
@@ -2,48 +2,47 @@ const display = document.querySelector(".text-content");
 let number1;
 let number2;
 let operator;
-// Adding
-function add(a, b) {
-    return a + b;
-}
-// Subtracting
-function subtract(a, b) {
-    return a - b;
-}
-// Multiplying
-function multiply(a, b) {
-    return a * b;
-}
-// Dividing
-function divide(a, b) {
-    return a / b;
-}
+
 // Take the two numbers and perform the operation
 function operate(num1, num2, oper) {
     let answer = 0;
     switch(oper) {
         case "+":
-            answer = add(num1, num2);
+            answer = num1 + num2;
             break;
         case "-":
-            answer = subtract(num1, num2);
+            answer = num1 - num2;
             break;
         case "*":
-            answer = multiply(num1, num2);
+            answer = num1 * num2;
             break;
         case "/":
-            answer = divide(num1, num2);
+            answer = num1 / num2;
             break;
     }
     return answer;
 }
 
 let numbers = document.querySelectorAll(".number");
-
 numbers.forEach((number) => {
     number.onclick = () => displayNumbers(number.innerHTML);
-})
-
+});
+// Display the number that is clicked onto the calculator display
 function displayNumbers(number) {
     display.textContent += number;
+}
+
+let maths = document.querySelectorAll(".operator");
+maths.forEach((op) => {
+    op.onclick = () => {
+        operator = op.innerHTML;
+        number1 = Number(display.innerHTML);
+        display.textContent = "";
+    };
+});
+
+const equals = document.querySelector(".equals");
+equals.onclick = () => {
+    number2 = Number(display.innerHTML);
+    display.textContent = operate(number1, number2, operator);
 }
