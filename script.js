1806a5ba8f0040a843085d80623061c835421adc
Liam Green <122028106+Spoogleey@users.noreply.github.com>
2024-10-01 11:37:41 +0100

    The display will clear for every operator
    Edited the code so that the display of the calculator will clear after clicking an operator button. The new number will then display on the calculator

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const display = document.querySelector(".text-content");
-let number1;
-let number2;
-let operator;
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+let resetDisplay = false;
 
 // Take the two numbers and perform the operation
 function operate(num1, num2, oper) {
@@ -19,30 +20,47 @@ function operate(num1, num2, oper) {
         case "/":
             answer = num1 / num2;
             break;
+        case "":
+            answer = num2;
+            break;
     }
     return answer;
 }
 
 let numbers = document.querySelectorAll(".number");
 numbers.forEach((number) => {
-    number.onclick = () => displayNumbers(number.innerHTML);
+    number.onclick = () => {
+        displayNumbers(number.innerHTML);
+        resetDisplay = false;
+    }
 });
 // Display the number that is clicked onto the calculator display
 function displayNumbers(number) {
+    if(resetDisplay) {
+        display.textContent = "";
+    }
     display.textContent += number;
 }
-
+// When an operator button is clicked
 let maths = document.querySelectorAll(".operator");
 maths.forEach((op) => {
     op.onclick = () => {
         operator = op.innerHTML;
         number1 = Number(display.innerHTML);
-        display.textContent = "";
+        resetDisplay = true;
     };
 });
-
+// When the equals button is clicked
 const equals = document.querySelector(".equals");
 equals.onclick = () => {
     number2 = Number(display.innerHTML);
     display.textContent = operate(number1, number2, operator);
 }
+// When the clear button is clicked
+const clear = document.querySelector(".clear");
+clear.onclick = () => {
+    number1 = 0;
+    number2 = 0;
+    operator = "";
+    display.textContent = "";
+};
